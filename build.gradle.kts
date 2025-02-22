25bb3b6381648e670dc60b49fe68109172576a40
JuHyun419 <zzang9ha@naver.com>
2025-02-22 17:36:55 +0900

    Initial commit (multi-module)

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+allprojects {
+    group = "jh"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("kotlin-kapt")
+        plugin("io.spring.dependency-management")
+        plugin("kotlin-spring")
+    }
+
+    dependencies {
+//        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+        // junit
+        testImplementation("org.junit.jupiter:junit-jupiter")
+    }
+
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "21"
+            }
+        }
+
+        withType<Test> {
+            useJUnitPlatform()
+        }
+    }
+}
