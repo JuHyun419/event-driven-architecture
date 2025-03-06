2458ffa495ca6aaf44b66ea153b21c743b4b687a
JuHyun419 <zzang9ha@naver.com>
2025-03-06 23:14:35 +0900

    config: adapter, api, usecase gradle

--- usecase/post-usecase/build.gradle.kts
+++ usecase/post-usecase/build.gradle.kts
@@ -1,2 +1,7 @@
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.3")
+
+    implementation(project(":usecase:core"))
+    implementation(project(":usecase:post-resolving-helper-usecase"))
+
 }
