7a6b17aee068372efcd076cdbb5a4529f8837ad9
JuHyun419 <zzang9ha@naver.com>
2025-03-07 00:26:19 +0900

    config: api module gradle add jackson dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter:3.4.3")
     implementation("org.springframework.boot:spring-boot-starter-web:3.4.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
     implementation(project(":common"))
