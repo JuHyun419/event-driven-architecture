acb116dd4b9deae56dcc2fa7a8ecf0811951265f
JuHyun419 <zzang9haha@gmail.com>
2025-04-02 23:21:42 +0900

    feat: add jackson module dependency in adapter module

--- adapter/build.gradle.kts
+++ adapter/build.gradle.kts
@@ -2,6 +2,7 @@ subprojects {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter:3.4.3")
         implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
         implementation(project(":common"))
         implementation(project(":domain"))
